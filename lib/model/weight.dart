efae4473df09ad22b28ba58c47243e6ad3092514
themik <themek93@gmail.com>
2022-12-29 16:43:39 +0100

    Added toString method

--- lib/model/weight.dart
+++ lib/model/weight.dart
@@ -4,5 +4,11 @@ class Weight {
   final DateTime dateEntry;
 
   Weight({required this.value, required this.dateEntry});
+
+
+  @override
+  String toString() {
+    return 'Weight + $value + $dateEntry';
+  }
   
 }
