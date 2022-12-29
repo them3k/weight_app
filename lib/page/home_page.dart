f6bb798585d2e79bb5b090739af659100a94ed39
themik <themek93@gmail.com>
2022-12-29 13:03:01 +0100

    added fab add weight

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weight_app/page/add_weight_page.dart';
 import 'package:weight_app/repository/weight_repository.dart';
 import 'package:weight_app/widget/weight_item.dart';
 
@@ -13,14 +14,18 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final items = repo.fetchFakeData();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page')
-      ),
-      body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (BuildContext context, int position){
-        return WeightItem(item: items[position]);
-      })
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push<MaterialPageRoute>(
+                MaterialPageRoute(builder: (context) => const AddWeightPage()));
+          },
+          child: const Icon(Icons.add),
+        ),
+        appBar: AppBar(title: const Text('Home Page')),
+        body: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (BuildContext context, int position) {
+              return WeightItem(item: items[position]);
+            }));
   }
 }
