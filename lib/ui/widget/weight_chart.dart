2a1bf137a31620390d662fb0cd9062b1e8ccd088
themik <themek93@gmail.com>
2023-02-03 08:23:51 +0100

    polish chart

--- lib/ui/widget/weight_chart.dart
+++ lib/ui/widget/weight_chart.dart
@@ -5,9 +5,7 @@ import 'package:flutter/material.dart';
 import '../../business_logic/utils/utils.date_format.dart';
 import '../../model/weight_model.dart';
 
-
-abstract class WeightChartWidget extends StatelessWidget{
-
+abstract class WeightChartWidget extends StatelessWidget {
   const WeightChartWidget({super.key});
 
   @override
@@ -16,39 +14,52 @@ abstract class WeightChartWidget extends StatelessWidget{
   LineChartData showLineChartData(List<Weight> weights) {
     List<FlSpot> spots = convertToDaysFlSpot(weights);
     DateTime now = DateTime.now();
-    spots.sort((a,b) => a.x.compareTo(b.x));
-    print('weight_chart | showLineChartData | weight.len: ${weights.length} | spots.len: ${spots.length}');
+    spots.sort((a, b) => a.x.compareTo(b.x));
+    print(
+        'weight_chart | showLineChartData | weight.len: ${weights.length} | spots.len: ${spots.length}');
     print('weight_chart | showLineChartData | spots: ${spots}');
     print('0.3: ${(weights.length * 0.3).floorToDouble()}');
     return LineChartData(
       borderData: FlBorderData(
-          border: const Border(bottom: BorderSide(), left: BorderSide())),
+          border:
+              Border(bottom: BorderSide(color: Colors.grey.withOpacity(0.4)))),
       gridData: FlGridData(
-          show: true, drawVerticalLine: true, horizontalInterval: 1,),
+          show: true,
+          drawVerticalLine: true,
+          verticalInterval: 1,
+          drawHorizontalLine: false),
       titlesData: FlTitlesData(
           show: true,
-          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          rightTitles: AxisTitles(
+              sideTitles: SideTitles(
+            interval: 1,
+            reservedSize: 62,
+            showTitles: true,
+            getTitlesWidget: rightTitleWidgets,
+          )),
           topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
           bottomTitles: AxisTitles(
               sideTitles: SideTitles(
-                interval: 1,
-                  showTitles: true,
-                  getTitlesWidget: (double value, TitleMeta meta) {
-                    return buildBottomTitleWidgets(value, meta, weights,now);
-                  },
-              )),
-          leftTitles: AxisTitles(
-              sideTitles: SideTitles(
-                  reservedSize: 28,
-                  showTitles: true,
-                  getTitlesWidget: leftTitleWidgets,
-              ))),
+            interval: 1,
+            showTitles: true,
+            getTitlesWidget: (double value, TitleMeta meta) {
+              return buildBottomTitleWidgets(value, meta, weights, now);
+            },
+          )),
+          leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false))),
       minY: countMinY(spots),
       maxY: countMaxY(spots),
       minX: countMinX(weights),
       maxX: countMaxX(weights),
       lineBarsData: [
         LineChartBarData(
+          belowBarData: BarAreaData(
+              show: true,
+              gradient: LinearGradient(
+                colors: [Colors.red, Colors.blueAccent]
+                    .map((color) => color.withOpacity(0.3))
+                    .toList(),
+              )),
           spots: spots,
           isCurved: true,
           barWidth: 4,
@@ -57,39 +68,46 @@ abstract class WeightChartWidget extends StatelessWidget{
     );
   }
 
-  Widget leftTitleWidgets(double value, TitleMeta meta){
-
+  Widget rightTitleWidgets(double value, TitleMeta meta) {
     if (value == meta.min) return Container();
-
-    return Text(value.toInt().toString());
+    print('Weight_chart | RightTitle | $value');
+    return Container(
+        margin: const EdgeInsets.only(left: 8),
+        child: Text(
+          '${value.toInt()} kg',
+          style: TextStyle(color: Colors.grey, fontSize: 12),
+        ));
   }
 
-  Widget buildBottomTitleWidgets(double value, TitleMeta meta, List<Weight> weights,DateTime now) {
-
+  Widget buildBottomTitleWidgets(
+      double value, TitleMeta meta, List<Weight> weights, DateTime now) {
     print('Weight_Chart | buildBottomTitle | $value');
     Widget xTitleWidget;
 
     if (value == meta.min) {
       xTitleWidget = showXTitle(weights[value.toInt()].dateEntry, now);
-    }else if (value == meta.max) {
-      xTitleWidget =  showXTitle(weights[value.toInt()].dateEntry, now);
-    }else if (value == (weights.length * 0.3).floorToDouble()) {
-      xTitleWidget =  showXTitle(weights[value.toInt()].dateEntry, now);
-    }else if (value == (weights.length * 0.6).toInt()) {
-      xTitleWidget =  showXTitle(weights[value.toInt()].dateEntry, now);
-    }else {
+    } else if (value == meta.max) {
+      xTitleWidget = showXTitle(weights[value.toInt()].dateEntry, now);
+    } else if (value == (weights.length * 0.3).floorToDouble()) {
+      xTitleWidget = showXTitle(weights[value.toInt()].dateEntry, now);
+    } else if (value == (weights.length * 0.6).toInt()) {
+      xTitleWidget = showXTitle(weights[value.toInt()].dateEntry, now);
+    } else {
       xTitleWidget = Container();
     }
     return xTitleWidget;
   }
 
   Widget showXTitle(DateTime weightDateTime, DateTime now) {
-    return Text(DateFormat.displayDateXAxis(weightDateTime, now));
+    return Container(
+      margin: const EdgeInsets.only(top: 8),
+        child: Text(
+      DateFormat.displayDateXAxis(weightDateTime, now),
+      style: TextStyle(color: Colors.grey, fontSize: 12),
+    ));
   }
 
-
-
-  double countMinY(List<FlSpot> spots){
+  double countMinY(List<FlSpot> spots) {
     List<double> ySpots = [];
     for (var spot in spots) {
       ySpots.add(spot.y);
@@ -105,21 +123,19 @@ abstract class WeightChartWidget extends StatelessWidget{
     return ySpots.reduce(max) + 2;
   }
 
-  double? countMinX(List<Weight> weights){
+  double? countMinX(List<Weight> weights) {
     return 0;
   }
 
-  double? countMaxX(List<Weight> weights){
-    return weights.length -1;
+  double? countMaxX(List<Weight> weights) {
+    return weights.length - 1;
   }
 
   List<FlSpot> convertToDaysFlSpot(List<Weight> weights) {
     List<FlSpot> spots = [];
     for (int i = 0; i < weights.length; i++) {
-      spots.add(FlSpot(i.toDouble(), weights[i].value));
+      spots.add(FlSpot(i.toDouble(), weights[i].value.floorToDouble()));
     }
-    return spots;
+    return spots.toSet().toList();
   }
-
-
 }
