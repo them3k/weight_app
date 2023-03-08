ec28c3066331559e162e99153cfbc6720aa29385
themik <themek93@gmail.com>
2023-03-08 19:33:32 +0100

    rebuild homeView

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -9,8 +9,10 @@ import 'package:weight_app/ui/views/home/widgets/congrat_widget.dart';
 import 'package:weight_app/ui/views/home/widgets/current_weight_widget.dart';
 import 'package:weight_app/ui/views/home/chart/widget/perdiod_segmented_buttons_widget.dart';
 
+import '../../../business_logic/view_model/home_view_model.dart';
 import '../../../model/periods.dart';
 import '../../../model/weight_model.dart';
+import '../../base_widget.dart';
 import '../../widget/chart_widget_from_7_days.dart';
 import '../add_page.dart';
 
@@ -28,29 +30,28 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    final appBarMaxHeight = Scaffold
-        .of(context)
-        .appBarMaxHeight ?? 56;
 
-    return ChangeNotifierProvider<WeightViewModel>.value(
-        value: WeightViewModel(),
-        child:
-        SizedBox(
-          height: mediaQuery.size.height -
-              kBottomNavigationBarHeight -
-              mediaQuery.padding.top -
-              appBarMaxHeight,
-          child: Column(
-            children: const [
-              CongratsWidget(),
-              CurrentWeightWidget(),
-            //  PeriodSegmentedButtonWidget(),
-              ChartContainerWidget(),
-              Spacer(),
-              AddWeightButtonContainer(),
-            ],
-          ),
-        ));
+    final appBarMaxHeight = Scaffold.of(context).appBarMaxHeight ?? 56;
+    return BaseWidget(
+        model: HomeViewModel(),
+        onModelReady: (model) => model.loadData(),
+        builder: (context, model, child) {
+          return SizedBox(
+            height: mediaQuery.size.height -
+                kBottomNavigationBarHeight -
+                mediaQuery.padding.top -
+                appBarMaxHeight,
+            child: Column(
+              children: [
+                CongratsWidget(gainWeightFromLastWeek:  model.gainedWeightFromLastWeek),
+                CurrentWeightWidget(),
+                ChartContainerWidget(),
+                Spacer(),
+                AddWeightButtonContainer(),
+              ],
+            ),
+          );
+        });
   }
 
 }
