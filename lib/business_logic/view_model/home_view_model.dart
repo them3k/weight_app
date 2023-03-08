748f0ee47fb52bd3483c4e3d3a04d243c3610e68
themik <themek93@gmail.com>
2023-03-08 17:03:27 +0100

    Added imports to home_view_model

--- lib/business_logic/view_model/home_view_model.dart
+++ lib/business_logic/view_model/home_view_model.dart
@@ -1,5 +1,10 @@
 import 'package:weight_app/business_logic/view_model/base_model.dart';
 
+import '../../model/weight_model.dart';
+import '../../service_locator.dart';
+import '../../services/storage/storage_service.dart';
+import '../utils/constants.dart';
+
 class HomeViewModel extends BaseModel {
 
 }
