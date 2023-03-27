9b71ac288a84a139be91d08908e62a76e958f2a8
themik <themek93@gmail.com>
2023-03-27 14:54:29 +0200

    created DI for chart_service_impl

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,6 +1,8 @@
 
 import 'package:get_it/get_it.dart';
 import 'package:weight_app/business_logic/view_model/weight_model.dart';
+import 'package:weight_app/services/chart_service/chart_service.dart';
+import 'package:weight_app/services/chart_service/chart_service_impl.dart';
 import 'package:weight_app/services/storage/storage_service.dart';
 import 'package:weight_app/services/storage/storage_service_fake.dart';
 import 'package:weight_app/services/storage/storage_service_impl.dart';
@@ -15,6 +17,7 @@ void setupServiceLocator() {
 
   // serviceLocator.registerFactory<WeightViewModel>(() => WeightViewModel());
   // serviceLocator.registerFactory<ChartViewModel>(() => ChartViewModel());
-  serviceLocator.registerLazySingleton<StorageService>(() => StorageServiceImpl());
+  serviceLocator.registerLazySingleton<StorageService>(() => StorageServiceFake());
+  serviceLocator.registerFactory<ChartService>(() => ChartServiceImpl());
 
 }
