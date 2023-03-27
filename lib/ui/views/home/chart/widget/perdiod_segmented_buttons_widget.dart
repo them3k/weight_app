4dfccfd49c068101a828671e81f16093e5b67e4d
themik <themek93@gmail.com>
2023-03-27 16:33:47 +0200

    polish segmentedButtons

--- lib/ui/views/home/chart/widget/perdiod_segmented_buttons_widget.dart
+++ lib/ui/views/home/chart/widget/perdiod_segmented_buttons_widget.dart
@@ -19,31 +19,31 @@ class _PeriodSegmentedButtonWidgetState
   @override
   Widget build(BuildContext context) {
     print('Perdiod_segmented | build');
-    const TextStyle textStyle = TextStyle(fontSize: 11);
-    return SegmentedButton<Periods>(
-      segments: const <ButtonSegment<Periods>>[
-        ButtonSegment<Periods>(
-            value: Periods.weekly, label: Text(Constants.WEEKLY_NAME_NUM,style: textStyle,)),
-        ButtonSegment<Periods>(
-            value: Periods.monthly, label: Text(Constants.MONTHLY_NAME_NUM,style: textStyle,)),
-        ButtonSegment<Periods>(
-            value: Periods.quarterly, label: Text(Constants.QUATERLY_NAME_NUM,style: textStyle,)),
-        ButtonSegment<Periods>(
-            value: Periods.semiAnnually, label: Text(Constants.SEMI_ANNUALY_NAME_NUM,style: textStyle,)),
-      ],
-      // [
-      //   ...periods.map((e) => ButtonSegment(
-      //       value: e,
-      //       label: Text('${e.name}',
-      //         style: textStyle,)))
-      selected: <Periods>{_period},
-      onSelectionChanged: (Set<Periods> newSelection) {
-        setState(() {
-          print('Period Segmented Button | onSelectionChanged');
-          _period = newSelection.first;
-        });
-        context.read<ChartsModel>().togglePeriod(newSelection.first);
-      },
+    const TextStyle textStyle = TextStyle(fontSize: 12,fontWeight: FontWeight.bold);
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 40),
+            child: SegmentedButton<Periods>(
+        showSelectedIcon: false,
+        segments: const <ButtonSegment<Periods>>[
+          ButtonSegment<Periods>(
+              value: Periods.weekly, label: Text(Constants.WEEKLY_NAME_NUM,style: textStyle,)),
+          ButtonSegment<Periods>(
+              value: Periods.monthly, label: Text(Constants.MONTHLY_NAME_NUM,style: textStyle,)),
+          ButtonSegment<Periods>(
+              value: Periods.quarterly, label: Text(Constants.QUATERLY_NAME_NUM,style: textStyle,)),
+          ButtonSegment<Periods>(
+              value: Periods.semiAnnually, label: Text(Constants.SEMI_ANNUALY_NAME_NUM,style: textStyle,)),
+        ],
+        selected: <Periods>{_period},
+        onSelectionChanged: (Set<Periods> newSelection) {
+          setState(() {
+            print('Period Segmented Button | onSelectionChanged');
+            _period = newSelection.first;
+          });
+          context.read<ChartsModel>().togglePeriod(newSelection.first);
+        },
+      ),
     );
   }
 }
