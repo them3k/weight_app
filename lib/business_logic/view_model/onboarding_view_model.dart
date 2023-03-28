56af4ed31b145083bf5ec1db2cf8eee6c5b29732
themik <themek93@gmail.com>
2023-03-28 08:57:46 +0200

    implement choice goal in onBoardingView

---
+++ lib/business_logic/view_model/onboarding_view_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:weight_app/service_locator.dart';
+import 'package:weight_app/services/storage/storage_service.dart';
+
+class OnBoardingViewModel extends ChangeNotifier {
+
+  final StorageService _storageService = serviceLocator<StorageService>();
+
+  void saveGoal(double value){
+    _storageService.saveGoal(value.truncateToDouble());
+  }
+}
