9c9e5fdf93d5bb2c64dd587c10440e329082b84c
themik <themek93@gmail.com>
2023-04-05 08:50:43 +0200

    created provider for app_state_management

---
+++ lib/business_logic/view_model/app_state_manager.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:weight_app/service_locator.dart';
+import 'package:weight_app/services/storage/storage_service.dart';
+
+class AppStateManagement extends ChangeNotifier {
+
+  final StorageService _storageService = serviceLocator<StorageService>();
+
+  bool _isInitialize = false;
+  bool _onBoardingComplete = false;
+  bool _onSetting = false;
+  bool _onHistory = false;
+
+
+  bool get isInitialize => _isInitialize;
+  bool get onBoardingComplete => _onBoardingComplete;
+  bool get onSetting => _onSetting;
+  bool get onHistory => _onHistory;
+
+  void isInitialized(bool value){
+    _isInitialize = value;
+    notifyListeners();
+  }
+
+  void onBoardingCompleteTapped(bool value){
+    _onBoardingComplete = value;
+    notifyListeners();
+  }
+
+  void onSettingTapped(bool value){
+    _onSetting = value;
+    notifyListeners();
+  }
+
+  void onHistoryTapped(bool value){
+    _onHistory = value;
+    notifyListeners();
+  }
+
+
+}
