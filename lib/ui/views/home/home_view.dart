cc6196783c4a254ced59b3e0ba2b3430df857221
themik <themek93@gmail.com>
2023-04-30 12:56:33 +0200

    Added static route method

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -4,11 +4,17 @@ import 'package:weight_app/ui/views/home/widgets/add_weight_button_container.dar
 import 'package:weight_app/ui/views/home/chart/widget/chart_view.dart';
 import 'package:weight_app/ui/views/home/widgets/congrat_widget.dart';
 import 'package:weight_app/ui/views/home/widgets/current_weight_widget.dart';
+import '../../../business_logic/utils/pages.dart';
 import '../../../business_logic/view_model/home_view_model.dart';
 import '../../base_widget.dart';
 import '../../widget/custom_app_bar.dart';
 
 class HomeView extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage<HomeView>(
+        key: ValueKey(Pages.homePath), name: Pages.homePath, child: HomeView());
+  }
+
   const HomeView({Key? key}) : super(key: key);
 
   @override
@@ -19,20 +25,20 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-
-    final appBarMaxHeight = Scaffold.of(context).appBarMaxHeight ?? 56;
-    return SizedBox(
-        height: mediaQuery.size.height -
-            kBottomNavigationBarHeight -
-            mediaQuery.padding.top,
-        child: Column(
-          children: const [
-            CustomAppBar(title: 'Home'),
-            HomeViewHeader(),
-            ChartView(),
-            Spacer(),
-            AddWeightButtonContainer()
-          ],
-        ));
+     return Scaffold(
+      body: SizedBox(
+          height: mediaQuery.size.height -
+              kBottomNavigationBarHeight -
+              mediaQuery.padding.top,
+          child: Column(
+            children: const [
+              CustomAppBar(title: 'Home'),
+              HomeViewHeader(),
+              ChartView(),
+              Spacer(),
+              AddWeightButtonContainer()
+            ],
+          )),
+    );
   }
 }
