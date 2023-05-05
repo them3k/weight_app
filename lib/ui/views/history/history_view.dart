934e54f004f02094fb842221172953e781ae0fcc
themik <themek93@gmail.com>
2023-05-05 08:52:09 +0200

    Added new bottomNavBar in HistoryView

--- lib/ui/views/history/history_view.dart
+++ lib/ui/views/history/history_view.dart
@@ -50,59 +50,63 @@ class _HistoryViewState extends State<HistoryView> {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     final listHeight = mediaQuery.size.height - kBottomNavigationBarHeight - 90;
-    return ProxyBaseWidget<HistoryViewModel, WeightModel>(
-      update: (context, parentModel, model) =>
-          model..loadData(parentModel.weights),
-      //onModelReady: (model) => model.loadData(),
-      model: HistoryViewModel(),
-      builder: (context, model, child) {
-        return Column(
-          children: [
-            HistoryAppBar(
-              onDelete: () {
-                context.read<WeightModel>().deleteWeights(model.selectedIndexes);
-                model.onTapDeleteSelectedItems();
-              },
-              title: 'History',
-              isItemSelected: model.isItemsSelected,
-            ),
-            model.busy
-                ? const CircularProgressIndicator()
-                : SizedBox(
-                    height: listHeight,
-                    child: ListView.builder(
-                        itemCount: model.weights.length,
-                        itemBuilder: (context, position) {
-                          return WeightItem(
-                            item: WeightPresentation.toPresentation(
-                                model.weights[position]),
-                            onLongItemPress: (int index) => {
-                              setState(() {
-                                context
-                                        .read<HistoryViewModel>()
-                                        .checkIfIsSelected(index)
-                                    ? context
-                                        .read<HistoryViewModel>()
-                                        .removeSelectedIndex(index)
-                                    : context
-                                        .read<HistoryViewModel>()
-                                        .selectItem(index);
-                              })
-                            },
-                            onItemPressed: (int position) {
-                              navigateToUpdatePage(
-                                  model.getItemAtIndex(position)!, position);
-                            },
-                            index: position,
-                            isPressed: model.checkIfIsSelected(position),
-                            isGreater:
-                                model.isWeightGrater(position, position - 1),
-                          );
-                        })),
-          ],
-        );
-      },
+    return Scaffold(
+      bottomNavigationBar: const BottomNavBar(),
+      body: ProxyBaseWidget<HistoryViewModel, WeightModel>(
+        update: (context, parentModel, model) =>
+        model..loadData(parentModel.weights),
+        //onModelReady: (model) => model.loadData(),
+        model: HistoryViewModel(),
+        builder: (context, model, child) {
+          return Column(
+            children: [
+              HistoryAppBar(
+                onDelete: () {
+                  context.read<WeightModel>().deleteWeights(model.selectedIndexes);
+                  model.onTapDeleteSelectedItems();
+                },
+                title: 'History',
+                isItemSelected: model.isItemsSelected,
+              ),
+              model.busy
+                  ? const CircularProgressIndicator()
+                  : SizedBox(
+                  height: listHeight,
+                  child: ListView.builder(
+                      itemCount: model.weights.length,
+                      itemBuilder: (context, position) {
+                        return WeightItem(
+                          item: WeightPresentation.toPresentation(
+                              model.weights[position]),
+                          onLongItemPress: (int index) => {
+                            setState(() {
+                              context
+                                  .read<HistoryViewModel>()
+                                  .checkIfIsSelected(index)
+                                  ? context
+                                  .read<HistoryViewModel>()
+                                  .removeSelectedIndex(index)
+                                  : context
+                                  .read<HistoryViewModel>()
+                                  .selectItem(index);
+                            })
+                          },
+                          onItemPressed: (int position) {
+                            navigateToUpdatePage(
+                                model.getItemAtIndex(position)!, position);
+                          },
+                          index: position,
+                          isPressed: model.checkIfIsSelected(position),
+                          isGreater:
+                          model.isWeightGrater(position, position - 1),
+                        );
+                      })),
+            ],
+          );
+        },
+      ) ,
     );
+
   }
 
   void navigateToUpdatePage(Weight item, int index) {
