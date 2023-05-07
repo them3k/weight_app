52dfc02a9ff4a9db3dda983dd167913506862c1d
themik <themek93@gmail.com>
2023-05-07 20:37:31 +0200

    Added weight as field

--- lib/business_logic/view_model/weight_manager.dart
+++ lib/business_logic/view_model/weight_manager.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../model/weight_model.dart';
+
 class WeightManager extends ChangeNotifier {
 
   int _selectedIndex = -1;
+  Weight? _weight;
 
   int get index => _selectedIndex;
 
-  void onChangeSelectedIndex(int index) {
-    if(_selectedIndex == index){
+  void onChangeSelectedWeightItem(int index, Weight weight) {
+    if(_selectedIndex == index || _weight == weight){
       return;
     }
     _selectedIndex = index;
+    _weight = weight;
     notifyListeners();
   }
 }
