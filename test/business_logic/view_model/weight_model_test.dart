7cb0a55bc576d68ee363251561cd487ec2ccba9b
themik <themek93@gmail.com>
2023-05-26 18:52:01 +0200

    wight model test set up

---
+++ test/business_logic/view_model/weight_model_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:weight_app/business_logic/view_model/weight_model.dart';
+import 'package:weight_app/model/weight_model.dart';
+import 'package:weight_app/service_locator.dart';
+import 'package:weight_app/services/storage/storage_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockServiceStorage extends Mock implements StorageService {}
+
+main() {
+  late WeightModel sut; // System under test
+
+  final weightsFromService = [
+    Weight(value: 25, dateEntry: DateTime(2023, 05, 03)),
+    Weight(value: 25, dateEntry: DateTime(2023, 05, 03)),
+    Weight(value: 25, dateEntry: DateTime(2023, 05, 03))
+  ];
+
+  final Weight testSingleWeight = Weight(
+      value: 25, dateEntry: DateTime(2023, 05, 03));
+
+
+  void arrangeStorageServiceReturns3Weights() {
+    when(() => sut.storageService.getWeightsByDate())
+        .thenAnswer((_) async => weightsFromService);
+  }
+
+  setUpAll(() {
+    serviceLocator.registerSingleton<StorageService>(MockServiceStorage());
+  });
+
+  setUp(() {
+    sut = WeightModel();
+  });
+}
